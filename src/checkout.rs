6f373a1e177cc5a6e1787d2624107c4bf5cbdb54
Max <maxledlie@outlook.com>
2023-06-19 21:50:09 +0100

    Checkout command copies files at a given commit into a given directory

--- src/checkout.rs
+++ src/checkout.rs
@@ -14,8 +14,8 @@ pub struct CheckoutArgs {
 
 pub fn cmd_checkout(args: CheckoutArgs, global_opts: GlobalOpts) -> Result<(), CmdError> {
     // Fail if the given directory is not empty
-    let path = PathBuf::from(args.directory);
-    let contents = fs::read_dir(&path).map_err(CmdError::IOError)?;
+    let destination = PathBuf::from(args.directory);
+    let contents = fs::read_dir(&destination).map_err(CmdError::IOError)?;
     
     if contents.into_iter().count() > 0 {
         return Err(CmdError::Fatal("Destination directory is not empty!".to_owned()));
@@ -30,28 +30,30 @@ pub fn cmd_checkout(args: CheckoutArgs, global_opts: GlobalOpts) -> Result<(), C
 
     // Parse the given commit object
     match search_object(&root, &hash, global_opts.git_mode) {
-        Ok(Some(Object::Commit(c))) => checkout_commit(&root, c, global_opts.git_mode),
+        Ok(Some(Object::Commit(c))) => checkout_commit(&root, c, &destination, global_opts.git_mode),
         Ok(Some(_)) => Err(CmdError::Fatal(String::from("Requested object is not a commit"))),
         Ok(None) => Err(CmdError::Fatal(String::from("Commit object does not exist"))),
         Err(e) => Err(e)
     }
 }
 
-fn checkout_commit(root: &PathBuf, commit: Commit, git_mode: bool) -> Result<(), CmdError> {
+fn checkout_commit(root: &PathBuf, commit: Commit, destination: &PathBuf, git_mode: bool) -> Result<(), CmdError> {
     match get_object(root, &commit.tree, git_mode) {
-        Ok(Object::Tree(t)) => checkout_tree(root, &t, git_mode),
+        Ok(Object::Tree(t)) => checkout_tree(root, &t, destination, git_mode),
         Ok(_) => Err(CmdError::Fatal(String::from("Commit references a tree that is not actually a tree"))),
         Err(e) => Err(e)
     }
 }
 
-fn checkout_tree(root: &PathBuf, tree: &Vec<GitTreeLeaf>, git_mode: bool) -> Result<(), CmdError> {
+fn checkout_tree(root: &PathBuf, tree: &Vec<GitTreeLeaf>, destination: &PathBuf, git_mode: bool) -> Result<(), CmdError> {
     for leaf in tree.into_iter() {
         println!("Checking out following tree node...");
         println!("{}", leaf);
 
+        let output_path = destination.join(&leaf.path);
+
         match get_object(root, &leaf.hash, git_mode) {
-            Ok(Object::Blob(_bytes)) => {}, // TODO: Write the file
+            Ok(Object::Blob(bytes)) => { fs::write(output_path, bytes).map_err(CmdError::IOError)?; },
             Ok(Object::Tree(_)) => {}, // TODO: Recurse on the subtree
             Ok(_) => return Err(CmdError::Fatal(String::from("Unexpected object found in tree. Expecting only blobs or trees"))),
             Err(e) => return Err(e)
